944afd2f2c79591b2c02b58835d0cd696dbec077
Larry Schirmer <larryschirmer@gmail.com>
2020-06-22 21:57:04 -0400

    add center logo

--- App/screens/Home/Home.styles.ts
+++ App/screens/Home/Home.styles.ts
@@ -1,10 +1,26 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 import { colors } from '../../constants';
+const screen = Dimensions.get("window");
 
 const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
     backgroundColor: colors.blue,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  logo: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  backgroundImage: {
+    width: screen.width * 0.45,
+    height: screen.height * 0.45,
+  },
+  foregroundImage: {
+    width: screen.width * 0.25,
+    height: screen.height * 0.25,
+    position: 'absolute'
   },
 });
 
--- App/screens/Home/Home.tsx
+++ App/screens/Home/Home.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { View, StatusBar } from 'react-native';
+import { View, StatusBar, Image } from 'react-native';
 
 import { colors } from '../../constants';
+
+import backgroundImage from '../../assets/images/background.png';
+import logoImage from '../../assets/images/logo.png';
 
 import styles from './Home.styles';
 
@@ -9,6 +12,10 @@ const Home = () => {
   return (
     <View style={styles.wrapper}>
       <StatusBar barStyle="light-content" backgroundColor={colors.blue} />
+      <View style={styles.logo}>
+        <Image style={styles.backgroundImage} source={backgroundImage} resizeMode="contain" />
+        <Image style={styles.foregroundImage} source={logoImage} resizeMode="contain" />
+      </View>
     </View>
   );
 };
